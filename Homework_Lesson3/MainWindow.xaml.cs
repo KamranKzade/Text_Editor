ab76cd38791b35fcb5701f8dc447f0132756a00a
Kamran <311.kamran@gmail.com>
2022-11-16 08:55:55 +0400

    Add ComboBox funksionalliqlari

--- Homework_Lesson3/MainWindow.xaml.cs
+++ Homework_Lesson3/MainWindow.xaml.cs
@@ -1,9 +1,17 @@
-﻿using System.Windows;
+﻿using System.Reflection;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Media;
 
 namespace Homework_Lesson3;
 
 public partial class MainWindow : Window
 {
+
+    private Color _textColor { get; set; }
+    private Color _backColor { get; set; }
+
+
     public MainWindow()
     {
         InitializeComponent();
@@ -16,6 +24,34 @@ public partial class MainWindow : Window
 
 
 
+
+
+
+
+
+
+    private void FontSize_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+    {
+        if (Textbox.Selection.IsEmpty)
+            Textbox.FontSize = double.Parse(FontSize_combo.SelectedItem.ToString()!);
+        else
+            Textbox.Selection.ApplyPropertyValue(TextElement.FontSizeProperty, FontSize_combo.SelectedItem.ToString());
+    }
+
+    private void FontFamily_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+    => Textbox.Selection.ApplyPropertyValue(TextElement.FontFamilyProperty, FontFamily.SelectedItem.ToString());
+
+    private void TextColor_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+    {
+        _textColor = (Color)(TextColor.SelectedItem as PropertyInfo)!.GetValue(null, null)!;
+        Textbox.Selection.ApplyPropertyValue(TextElement.ForegroundProperty, new SolidColorBrush(_textColor));
+    }
+
+    private void BackColor_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+    {
+        _backColor = (Color)(BackColor.SelectedItem as PropertyInfo)!.GetValue(null, null)!;
+        Textbox.Background = new SolidColorBrush(_backColor);
+    }
 
     private void ZoomAdd()
     {
@@ -32,9 +68,9 @@ public partial class MainWindow : Window
         for (int i = 8; i <= 72; i++)
         {
             if (i % 2 == 0)
-                FontSize.Items.Add(i);
+                FontSize_combo.Items.Add(i);
         }
-
-        FontSize.SelectedIndex = 0;
     }
+
+
 }
