78cb647fe890791e14c542ffe2a86824ddada0f0
Kamran <311.kamran@gmail.com>
2022-11-16 09:57:08 +0400

    Complete without Dialog

--- Homework_Lesson3/MainWindow.xaml.cs
+++ Homework_Lesson3/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System.Reflection;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Documents;
 using System.Windows.Media;
 
@@ -53,6 +55,36 @@ public partial class MainWindow : Window
         Textbox.Background = new SolidColorBrush(_backColor);
     }
 
+    private void Zoom_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+    {
+        window.Height = 700;
+        window.Width = 1000;
+
+        window.Height = window.Height * double.Parse(Zoom.SelectedItem.ToString())! / 100!;
+        window.Width = window.Width * double.Parse(Zoom.SelectedItem.ToString())! / 100;
+    }
+
+    private void Left_Click(object sender, RoutedEventArgs e)
+    {
+        var btn = sender as Button;
+
+        switch (btn!.Name)
+        {
+            case "Right":
+                Textbox.Document.TextAlignment = TextAlignment.Right;
+                break;
+            case "Center":
+                Textbox.Document.TextAlignment = TextAlignment.Center;
+                break;
+            case "Justify":
+                Textbox.Document.TextAlignment = TextAlignment.Justify;
+                break;
+            default:
+                Textbox.Document.TextAlignment = TextAlignment.Left;
+                break;
+        }
+    }
+
     private void ZoomAdd()
     {
         Zoom.Items.Add("50");
@@ -72,5 +104,55 @@ public partial class MainWindow : Window
         }
     }
 
+    private void Bold_Click(object sender, RoutedEventArgs e)
+    {
+        var btn = sender as ToggleButton;
+
+        if (btn.IsChecked == true)
+        {
+            btn.Foreground = new SolidColorBrush(Colors.Red);
+            switch (btn!.Name)
+            {
+                case "Bold":
+                    // Textbox.FontWeight = FontWeights.Bold;
+                    Textbox.Selection.ApplyPropertyValue(FontWeightProperty, FontWeights.Bold);
+                    break;
+                case "Italic":
+                    // Textbox.FontStyle = FontStyles.Italic;
+                    Textbox.Selection.ApplyPropertyValue(FontStyleProperty, FontStyles.Italic);
+                    break;
+                case "Ulderline":
+                    Textbox.Selection.ApplyPropertyValue(Inline.TextDecorationsProperty, TextDecorations.Underline);
+                    break;
+                default:
+                    break;
+            }
+        }
+        else
+        {
+            // Textbox.FontWeight = FontWeights.Normal;
+            // Textbox.FontStyle = FontStyles.Normal;
+            btn.Foreground = new SolidColorBrush(Colors.Black);
+            Textbox.Selection.ApplyPropertyValue(FontWeightProperty, FontWeights.Normal);
+            Textbox.Selection.ApplyPropertyValue(FontStyleProperty, FontStyles.Normal);
+            Textbox.Selection.ApplyPropertyValue(Underline.TextDecorationsProperty, TextDecorations.Baseline);
+        }
+    }
+
+    private void Undo_Click(object sender, RoutedEventArgs e)
+    {
+        var btn = sender as Button;
+        switch (btn!.Name)
+        {
+            case "Undo":
+                Textbox.Undo();
+                break;
+            case "Redo":
+                Textbox.Redo();
+                break;
+            default:
+                break;
+        }
+    }
 
 }
