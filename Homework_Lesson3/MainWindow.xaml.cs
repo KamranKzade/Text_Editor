b8edf290099c194f7d5e9ed8b6708e9689a76f7a
Kamran <311.kamran@gmail.com>
2022-11-16 08:24:39 +0400

    Add project files.

---
+++ Homework_Lesson3/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+
+namespace Homework_Lesson3;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        FontSizeAdd();
+        ZoomAdd();
+    }
+
+
+
+
+
+
+
+    private void ZoomAdd()
+    {
+        Zoom.Items.Add("50");
+        Zoom.Items.Add("75");
+        Zoom.Items.Add("100");
+        Zoom.Items.Add("125");
+        Zoom.Items.Add("150");
+        Zoom.Items.Add("200");
+    }
+
+    private void FontSizeAdd()
+    {
+        for (int i = 8; i <= 72; i++)
+        {
+            if (i % 2 == 0)
+                FontSize.Items.Add(i);
+        }
+
+        FontSize.SelectedIndex = 0;
+    }
+}
